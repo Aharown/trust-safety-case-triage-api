b6795a635e936c168550127d497c43c0d91a6acd
Aaron Boakye <cheminko@hotmail.com>
2026-09-01 07:39:03 +0100

    Added casecreate schema and POST create case endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.models import Case
-from app.schemas import CaseResponse
+from app.models import Case, CaseState
+from app.schemas import CaseResponse, CaseCreate
 
 app = FastAPI()
 
@@ -10,3 +10,12 @@ app = FastAPI()
 @app.get("/cases", response_model=list[CaseResponse])
 def get_cases(db: Session = Depends(get_db)):
     return db.query(Case).all()
+
+
+@app.post("/cases", response_model=CaseResponse)
+def create_case(case: CaseCreate, db: Session = Depends(get_db)):
+    new_case = Case(description=case.description, state=CaseState.new)
+    db.add(new_case)
+    db.commit()
+    db.refresh(new_case)
+    return new_case
